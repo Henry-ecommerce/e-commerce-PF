b9014fffd2fd32e064f39516e04b454d59a887d6
ManuMxrales <manumxrales@gmail.com>
2022-06-08 14:06:27 -0500

    Trabajando estilos del componente review

--- client/src/Components/Review/Review.jsx
+++ client/src/Components/Review/Review.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import {
   FormControl,
   FormLabel,
@@ -18,19 +18,33 @@ import {
 import { AiFillStar } from "react-icons/ai";
 
 const Review = () => {
-  const [rating, setRating] = useState(0);
+  let RegExpression = /^[a-zA-Z\s]*$/;
+  let message = useRef("");
   const stars = [];
+  const [mensajeEnviado, setMensajeEnviado] = useState("");
+  const [rating, setRating] = useState(0);
+  const [input, setInput] = useState({
+    titulo: "",
+    rating: 0,
+    description: "",
+  });
 
-  const StarIcon = () => (
-    <Icon as={AiFillStar} boxSize={8} color="rgba( 248, 231, 28, 1 )" />
+  const StarIcon = ({ fill }) => (
+    <Icon
+      as={AiFillStar}
+      boxSize={8}
+      fillOpacity={fill ? "100%" : "25%"}
+      color="orange"
+    />
   );
-  const StarButton = (idx) => {
+
+  const StarButton = ({ idx, fill }) => {
     return (
       <IconButton
         variant="outline"
         aria-label={`Rate ${idx}`}
-        icon={<StarIcon />}
-        onClick={() => onRating(idx)}
+        icon={<StarIcon fill={fill} />}
+        onClick={() => onClick(idx)}
       />
     );
   };
@@ -38,40 +52,96 @@ const Review = () => {
   for (let i = 1; i <= 5; i++) {
     stars.push(<StarButton key={i} idx={i} fill={i <= rating} />);
   }
-  const onRating = (idx) => {
+
+  const onClick = (idx) => {
     if (!isNaN(idx)) {
-      // allow user to click first icon and set rating to zero if rating is already 1
       if (rating === 1 && idx === 1) {
         setRating(0);
+        setInput({
+          ...input,
+          rating: 0,
+        });
       } else {
         setRating(idx);
-        console.log(rating + "entro");
+        setInput({
+          ...input,
+          rating: idx,
+        });
       }
     }
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(input);
+    if (input.titulo.length < 3 || input.titulo.trim().length === 0) {
+      setMensajeEnviado("Informacion Insuficiente");
+    } else {
+      // dispatch(action(input))
+      setInput({
+        titulo: "",
+        rating: rating,
+        description: "",
+      });
+      setMensajeEnviado("Review Creada con Exito!");
+    }
+  };
+
+  const handleInputCheck = (e) => {
+    let inputCheck = e.target.value;
+    if (!RegExpression.test(inputCheck)) {
+      message.current = "Caracteres No Validos Por favor Verifica";
+    }
+    if (inputCheck.length === 1) {
+      message.current = "";
+    }
+
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
-    <Flex mt="30px" align={"center"} justify={"center"}>
-      <Box bg="#242525" color="#ECEDEC" p="10px" fontSize={"2xl"} m="10">
+    <Flex m="30px" align={"center"} justify={"center"}>
+      <Box color="#ECFDEC" bg="#1C1C1C" p="16px" fontSize={"2xl"} m="10" rounded="lg">
+        <h1>{mensajeEnviado}</h1>
+        <form onSubmit={(e) => handleSubmit(e)}>
           <FormControl>
             <FormLabel htmlFor="title">Titulo</FormLabel>
-            <FormHelperText>
-              ¿Qué es lo que quieres compartir del producto?
+            <FormHelperText color="#ECFDEC">
+              {message.current.length > 1
+                ? `${message.current}`
+                : "¿Qué es lo que quieres compartir del producto?"}
             </FormHelperText>
-            <Input id="title" type="text" />
+            <Input
+              id="title"
+              name="titulo"
+              type="text"
+              onChange={(e) => handleInputCheck(e)}
+              isRequired
+            />
             <FormLabel htmlFor="rating">Rating</FormLabel>
-            <input name="rating" type="hidden" value={rating} />
-            {stars}
+            <p>{rating}</p>
+            <Flex align={"center"} justify={"center"}>
+              {stars}
+            </Flex>
             <FormLabel htmlFor="comentario">
               ¡Cuéntanos más sobre el producto!
             </FormLabel>
-            <FormHelperText>
+            <FormHelperText color="#ECFDEC">
               ¿Qué te gusto o que no te gusto? ¿Para qué usaste el producto?
             </FormHelperText>
-            <Textarea id="comentario" />
+            <Textarea
+              id="comentario"
+              name="description"
+              onChange={(e) => handleInputCheck(e)}
+              maxLength="280"
+              isRequired
+            />
             <Button
               type="submit"
-              loadingText="Submitting"
+              loadingText="Enviando"
               size="lg"
               bg={"black"}
               color={"white"}
@@ -83,6 +153,7 @@ const Review = () => {
               Enviar
             </Button>
           </FormControl>
+        </form>
       </Box>
     </Flex>
   );
