1c0edc45a0c80adcc3705d71378ccd5cf8915731
ManuMxrales <manumxrales@gmail.com>
2022-06-11 15:07:47 -0500

    Se agrego ruta Post Y Get para reviews

--- api/src/Routes/index.js
+++ api/src/Routes/index.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const Productos = require("./Admin/Productos");
 const Filters = require("./Filters/indexFilters")
+const Review = require("./Review/review.router.js")
 
 const router = Router();
 
 router.use("/productos", Productos);
 router.use("/filter", Filters) // filtros y ORDENAMIENTOS
-
+router.use("/review", Review); // POST Y GET de Reviews
 
 module.exports = router;
