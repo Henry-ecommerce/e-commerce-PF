a7b5b6e252289f342d96b4b799bcb9e1d9b2a180
ManuMxrales <manumxrales@gmail.com>
2022-06-11 15:17:09 -0500

    Se arreglo la relacion para la tabla reviews NOTA se cambio el puerto a 5432 cambialo si es necesario

--- api/src/db.js
+++ api/src/db.js
@@ -33,7 +33,7 @@ let sequelize =
 				ssl: true,
 		  })
 		: new Sequelize(
-				`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5001/${DB_NAME}`,
+				`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/${DB_NAME}`,
 				{
 					logging: false, // set to console.log to see the raw SQL queries
 					native: false, // lets Sequelize know we can use pg-native for ~30% more speed
@@ -59,7 +59,7 @@ Historial.hasOne(Usuario);
 Usuario.hasMany(Review);
 Review.hasOne(Usuario);
 Producto.hasMany(Review);
-Review.hasOne(Producto);
+Review.belongsTo(Producto);
 
 Usuario.belongsToMany(Producto,{through : "Favoritos"})
 Producto.belongsToMany(Usuario,{through : "Favoritos"})
