8b23c7e9cfb70dbd5ce9f767133f66dcdd5aee10
LeonelR33 <98905940+LeonelR33@users.noreply.github.com>
2022-06-13 17:35:56 -0300

    Update indexCategories.js

--- api/src/Routes/Categories/indexCategories.js
+++ api/src/Routes/Categories/indexCategories.js
@@ -4,22 +4,22 @@ const Sequelize = require('sequelize');
 const router = Router();
 
 let _categorias = [
-    {nombre: "Tarjeta Grafica", id: 1,imagen: "https://www.cyberpuerta.mx/out/pictures/master/category/icon/tdv(1).png"},
-    {nombre: "Mousepads", id: 2, imagen:"https://www.cyberpuerta.mx/img/product/M/CP-YEYIAN-MP2035-1.jpg",},
-    {nombre: "Microfonos", id: 3, imagen: "https://www.cyberpuerta.mx/img/product/M/CP-RAZER-RZ19-02290400-R3M1-1.jpg",},
-    {nombre: "Audifonos", id: 4, imagen: "https://www.cyberpuerta.mx/img/product/M/CP-NACEBTECHNOLOGY-NA-0317-1.jpg",},
-    {nombre: "Gabinetes", id: 5, imagen: "https://www.cyberpuerta.mx/img/product/XL/CP-MSI-MPGGUNGNIR110R-3.jpg",},
-    {nombre: "Disipadores-para-CPU", id: 6, imagen: "https://www.cyberpuerta.mx/img/product/M/CP-BALAMRUSH-BR-931311-1.jpg",},
-    {nombre: "Ventiladores", id: 7, imagen: "https://www.cyberpuerta.mx/img/product/M/CP-INWIN-ASL120FAN-3PK-1.jpg",},
-    {nombre: "SSD", id: 8, imagen: "https://www.cyberpuerta.mx/out/pictures/master/category/icon/ssds.png",},
-    {nombre: "Tarjetas-Madre", id: 9, imagen: "https://www.cyberpuerta.mx/out/pictures/master/category/icon/motherboard2.png",},
-    {nombre: "PC-s-Gamer", id: 10, imagen: "https://www.cyberpuerta.mx/out/pictures/master/category/icon/pc-escritorio.png",},
-    {nombre: "Procesadores", id: 11, imagen: "https://www.cyberpuerta.mx/out/pictures/master/category/icon/procesador-ryzen.png",},
-    {nombre: "Mouse", id: 12, imagen: "https://www.cyberpuerta.mx/img/product/M/CP-LOGITECH-910-004284-1.jpg",},
-    {nombre: "Discos-Duros", id: 13, imagen: "https://www.cyberpuerta.mx/out/pictures/master/category/icon/dd-nas.png",},
-    {nombre: "Fuentes-de-Poder-para-PC-s", id: 14, imagen: "https://www.cyberpuerta.mx/img/product/M/CP-GIGABYTE-GP-P750GM-1.jpg",},
-    {nombre: "Teclados", id: 15, imagen: "https://www.cyberpuerta.mx/img/product/M/CP-VORAGO-KB-503-1.jpg"},
-    {nombre: "Monitores", id: 16, imagen: "https://www.cyberpuerta.mx/out/pictures/master/category/icon/monitores(1).png",},
+    {name: "Tarjeta Grafica", id: 1,imagen: "https://www.cyberpuerta.mx/out/pictures/master/category/icon/tdv(1).png"},
+    {name: "Mousepads", id: 2, imagen:"https://www.cyberpuerta.mx/img/product/M/CP-YEYIAN-MP2035-1.jpg",},
+    {name: "Microfonos", id: 3, imagen: "https://www.cyberpuerta.mx/img/product/M/CP-RAZER-RZ19-02290400-R3M1-1.jpg",},
+    {name: "Audifonos", id: 4, imagen: "https://www.cyberpuerta.mx/img/product/M/CP-NACEBTECHNOLOGY-NA-0317-1.jpg",},
+    {name: "Gabinetes", id: 5, imagen: "https://www.cyberpuerta.mx/img/product/XL/CP-MSI-MPGGUNGNIR110R-3.jpg",},
+    {name: "Disipadores-para-CPU", id: 6, imagen: "https://www.cyberpuerta.mx/img/product/M/CP-BALAMRUSH-BR-931311-1.jpg",},
+    {name: "Ventiladores", id: 7, imagen: "https://www.cyberpuerta.mx/img/product/M/CP-INWIN-ASL120FAN-3PK-1.jpg",},
+    {name: "SSD", id: 8, imagen: "https://www.cyberpuerta.mx/out/pictures/master/category/icon/ssds.png",},
+    {name: "Tarjetas-Madre", id: 9, imagen: "https://www.cyberpuerta.mx/out/pictures/master/category/icon/motherboard2.png",},
+    {name: "PC-s-Gamer", id: 10, imagen: "https://www.cyberpuerta.mx/out/pictures/master/category/icon/pc-escritorio.png",},
+    {name: "Procesadores", id: 11, imagen: "https://www.cyberpuerta.mx/out/pictures/master/category/icon/procesador-ryzen.png",},
+    {name: "Mouse", id: 12, imagen: "https://www.cyberpuerta.mx/img/product/M/CP-LOGITECH-910-004284-1.jpg",},
+    {name: "Discos-Duros", id: 13, imagen: "https://www.cyberpuerta.mx/out/pictures/master/category/icon/dd-nas.png",},
+    {name: "Fuentes-de-Poder-para-PC-s", id: 14, imagen: "https://www.cyberpuerta.mx/img/product/M/CP-GIGABYTE-GP-P750GM-1.jpg",},
+    {name: "Teclados", id: 15, imagen: "https://www.cyberpuerta.mx/img/product/M/CP-VORAGO-KB-503-1.jpg"},
+    {name: "Monitores", id: 16, imagen: "https://www.cyberpuerta.mx/out/pictures/master/category/icon/monitores(1).png",},
 ]
 
 router.post("/create", async (req, res) => {
