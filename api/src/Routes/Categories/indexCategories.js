845c59f7ccc2dde82efc79f3b83cb2809ee3beeb
Leonel Retamar <leonel.retamar33@gmail.com>
2022-06-13 17:34:00 -0300

    esta es la correcta xd

--- api/src/Routes/Categories/indexCategories.js
+++ api/src/Routes/Categories/indexCategories.js
@@ -4,22 +4,22 @@ const Sequelize = require('sequelize');
 const router = Router();
 
 let _categorias = [
-    { id:1, name: "Tarjeta Grafica"},
-    { id:2, name: "Mousepads"},
-    { id:3, name: "Microfonos"},
-    { id:4, name: "Audifonos"},
-    { id:5, name: "Gabinetes"},
-    { id:6, name: "Disipadores para CPU"},
-    { id:7, name: "Ventiladores"},
-    { id:8, name: "SSD"},
-    { id:9, name: "Tarjetas Madre"},
-    { id:10, name: "PCs Gamer"},
-    { id:11, name: "Procesadores"},
-    { id:12, name: "Mouse"},
-    { id:13, name: "Discos-Duros"},
-    { id:14, name: "Fuentes de Poder para PCs"},
-    { id:15, name: "Teclados"},
-    { id:16, name: "Monitores"},
+    {nombre: "Tarjeta Grafica", id: 1,imagen: "https://www.cyberpuerta.mx/out/pictures/master/category/icon/tdv(1).png"},
+    {nombre: "Mousepads", id: 2, imagen:"https://www.cyberpuerta.mx/img/product/M/CP-YEYIAN-MP2035-1.jpg",},
+    {nombre: "Microfonos", id: 3, imagen: "https://www.cyberpuerta.mx/img/product/M/CP-RAZER-RZ19-02290400-R3M1-1.jpg",},
+    {nombre: "Audifonos", id: 4, imagen: "https://www.cyberpuerta.mx/img/product/M/CP-NACEBTECHNOLOGY-NA-0317-1.jpg",},
+    {nombre: "Gabinetes", id: 5, imagen: "https://www.cyberpuerta.mx/img/product/XL/CP-MSI-MPGGUNGNIR110R-3.jpg",},
+    {nombre: "Disipadores-para-CPU", id: 6, imagen: "https://www.cyberpuerta.mx/img/product/M/CP-BALAMRUSH-BR-931311-1.jpg",},
+    {nombre: "Ventiladores", id: 7, imagen: "https://www.cyberpuerta.mx/img/product/M/CP-INWIN-ASL120FAN-3PK-1.jpg",},
+    {nombre: "SSD", id: 8, imagen: "https://www.cyberpuerta.mx/out/pictures/master/category/icon/ssds.png",},
+    {nombre: "Tarjetas-Madre", id: 9, imagen: "https://www.cyberpuerta.mx/out/pictures/master/category/icon/motherboard2.png",},
+    {nombre: "PC-s-Gamer", id: 10, imagen: "https://www.cyberpuerta.mx/out/pictures/master/category/icon/pc-escritorio.png",},
+    {nombre: "Procesadores", id: 11, imagen: "https://www.cyberpuerta.mx/out/pictures/master/category/icon/procesador-ryzen.png",},
+    {nombre: "Mouse", id: 12, imagen: "https://www.cyberpuerta.mx/img/product/M/CP-LOGITECH-910-004284-1.jpg",},
+    {nombre: "Discos-Duros", id: 13, imagen: "https://www.cyberpuerta.mx/out/pictures/master/category/icon/dd-nas.png",},
+    {nombre: "Fuentes-de-Poder-para-PC-s", id: 14, imagen: "https://www.cyberpuerta.mx/img/product/M/CP-GIGABYTE-GP-P750GM-1.jpg",},
+    {nombre: "Teclados", id: 15, imagen: "https://www.cyberpuerta.mx/img/product/M/CP-VORAGO-KB-503-1.jpg"},
+    {nombre: "Monitores", id: 16, imagen: "https://www.cyberpuerta.mx/out/pictures/master/category/icon/monitores(1).png",},
 ]
 
 router.post("/create", async (req, res) => {
@@ -28,6 +28,7 @@ router.post("/create", async (req, res) => {
             Categoria.create({
                 id: el.id,
                 nombre: el.name,
+                imagen: el.imagen,
             })
         })
         let categorias = await Categoria.findAll();
