dc64ed235c80b37b27f49181eb1674b8489e7d5f
Leonel Retamar <leonel.retamar33@gmail.com>
2022-06-13 22:30:58 -0300

    edite los nombres, tenian guiones

---
+++ client/src/Components/CategoryCard/CategoryCard.jsx
@@ -0,0 +1,22 @@
+import { Box, Flex, Image } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+
+const CategoryCard = ({nombre, imagen}) => {
+
+
+
+    //quite el nombre (${nombre}) del link para evitar que rompa ya que las categorias aun no estan integradas.
+    return (
+    <>
+    <Box width="300px" height="150px" bg="white" display="inline-block" borderRadius="15px">
+        <Flex m="5px" mt="10px" alignItems="center" flexDirection="column" fontSize="15px" fontWeight="700">
+        <Box _hover={{cursor: "pointer"}} maxHeight="150px" maxWidth="150px"><Link to={`/products`}><Image boxSize="115px" objectFit="scale-down" src={imagen} alt={nombre}/></Link></Box>
+        <p>{nombre?.replace(/-/g," ")}</p>
+        </Flex>
+    </Box>
+    </>
+    )
+}
+
+export default CategoryCard;
