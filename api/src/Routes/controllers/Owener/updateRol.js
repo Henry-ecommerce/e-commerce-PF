9667ac19f309a7f489b57097d3846bb42334ceeb
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-06-20 17:37:46 -0500

    Baneos update rango

--- api/src/Routes/controllers/Owener/updateRol.js
+++ api/src/Routes/controllers/Owener/updateRol.js
@@ -1,18 +1,32 @@
 const { Router } = require("express");
-const { Registro } = require("../../../db");
+const { Usuario } = require("../../../db");
 
 const router = Router();
 
 router.put("/:id", async (req, res) => {
-  const { rol } = req.body;
   const { id } = req.params;
-  const obtenerUser = await Registro.findByPk(id);
+  const { rols } = req.body;
+
+  const obtenerUser = await Usuario.findByPk(id);
 
   if (!obtenerUser) {
     const error = new Error("El usuario no se encontro");
     return res.status(400).json({ msg: error.message });
   }
-  obtenerUser.rol = rol;
+
+  if (rols === "admin") {
+    obtenerUser.rol = "Admin";
+    obtenerUser.save();
+    return res.status(202).json({
+      msg: `La cuenta con el nombre ${obtenerUser.name} ahora tiene el rol de administrador`,
+    });
+  } else if (rols === "user") {
+    obtenerUser.rol = "User";
+    obtenerUser.save();
+    return res.status(202).json({
+      msg: `La cuenta con el nombre ${obtenerUser.name} ahora tiene el rol de usuario`,
+    });
+  }
   const nuevoRol = obtenerUser.save();
 
   res.status(202).json(nuevoRol);
