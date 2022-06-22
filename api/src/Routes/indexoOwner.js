7b6e01a95eb853a0574b564295e99d6840df36f8
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-06-21 22:47:22 -0500

    Baneos-updateRoles-frond

--- api/src/Routes/indexoOwner.js
+++ api/src/Routes/indexoOwner.js
@@ -6,6 +6,11 @@ const obtenerProducto = require("./controllers/admin_productos/obtenerProductos"
 const updateModificarProducto = require("./controllers/admin_productos/updateProductos");
 const borrarBorrarProducto = require("./controllers/admin_productos/borrarProductos");
 const getUsers = require("./controllers/admin_productos/getUsers");
+const getperfilUse = require("./controllers/Owener/perfilUser");
+const getTransaccioness = require("./controllers/admin_productos/transaciones");
+const getventas = require("./controllers/admin_productos/ventas");
+const getOrdenes = require("./controllers/admin_productos/ordenes");
+const getCategorias = require("./controllers/admin_productos/categoriasProduc");
 const { checkAuth } = require("../middleware/authMiddleware");
 const { owner } = require("../middleware/Owner");
 
@@ -18,5 +23,9 @@ router.use("/obtener", [checkAuth, owner], obtenerProducto);
 router.use("/modificar", [checkAuth, owner], updateModificarProducto);
 router.use("/borrar", [checkAuth, owner], borrarBorrarProducto);
 router.use("/usuarios", [checkAuth, owner], getUsers);
-
+router.use("/perfilUser", [checkAuth, owner], getperfilUse);
+router.use("/transacciones", [checkAuth, owner], getTransaccioness);
+router.use("/ventas", [checkAuth, owner], getventas);
+router.use("/categorias", [checkAuth, owner], getCategorias);
+router.use("/ordenes", [checkAuth, owner], getOrdenes);
 module.exports = router;
