052c8087df707aaa6cc361b4815ddd91f985147d
Tomas Shanahan <tshmg@hotmail.com>
2022-06-27 19:57:31 -0300

    carousel de productos mas vendidos

---
+++ api/src/Routes/Admin/Pedidos.js
@@ -0,0 +1,32 @@
+const { Router } = require("express");
+const router = Router();
+const { Usuario, Pedido, Producto } = require("../../db");
+
+router.get("/", async (req, res) => {
+	let pedidos = await Pedido.findAll({
+		include: Usuario,
+		order: [["id", "DESC"]],
+	});
+	pedidos = await pedidos.map((e) => e.items).flat(Infinity);
+
+	let productos = await Producto.findAll({
+		where: { id: pedidos.map((e) => e.id) },
+    order : [["id",'ASC']]
+	});
+
+	let obj = {};
+	for (let i = 0; i < pedidos.length; i++) {
+		if (obj.hasOwnProperty(pedidos[i].id)) {
+			obj[pedidos[i].id] += 1;
+		} else {
+			obj[pedidos[i].id] = 1;
+		}
+	}
+
+	let productos_y_cantidad_de_ventas = [];
+	Object.entries(obj).map((e, i) => productos[i]?.id === Number(e[0]) && productos_y_cantidad_de_ventas.push({ ...productos[i].dataValues, total_en_ventas: e[1] })); //?
+	
+	res.json(productos_y_cantidad_de_ventas);
+});
+
+module.exports = router;
--- api/src/Routes/index.js
+++ api/src/Routes/index.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const Productos = require("./Admin/Productos");
+const Pedidos = require("./Admin/Pedidos");
 const Filters = require("./Filters/indexFilters")
 const Review = require("./Review/review.router.js")
 const Categorias = require("./Categories/indexCategories")
@@ -11,5 +12,6 @@ router.use("/filter", Filters) // filtros y ORDENAMIENTOS
 router.use("/review", Review); // POST Y GET de Reviews
 router.use("/categorias", Categorias);
 router.use("/chatbot", Chatbot);
+router.use("/pedidos", Pedidos);
 
 module.exports = router;
